3a89d66fa91371faa48b42101419ad28a74aac94
09Kanika <kanika2005dogra@gmail.com>
2024-05-29 22:36:31 +0530

    first commit

---
+++ chatbot.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model=genai.GenerativeModel("gemini-pro")
+chat=model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response=chat.send_message(question,stream=True)
+    return response
+
+st.set_page_config(page_title="Q&A Chatbot")
+st.header("Conversational Q&A chatbot 💬🤖")
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
+
+input=st.text_input("Input: ",key="input")
+submit=st.button("Ask question")
+
+if submit and input:
+    response=get_gemini_response(input)
+    st.session_state['chat_history'].append(("You",input))
+    st.subheader("The Response is: ")
+    for chunk in response:
+        st.write(chunk.text)
+
+        st.session_state['chat_history'].append(("You",chunk.text))
+
+st.subheader("History: ")
+for role,text in st.session_state['chat_history']:
+    st.write(f"{role}:{text}")
